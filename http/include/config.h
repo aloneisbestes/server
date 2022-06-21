516aa56dc53965967502e809936f194e3bdfc66b
aloneisbestes <aloneisbestes@gmail.com>
2022-06-21 19:49:18 +0800

    config file

---
+++ http/include/config.h
@@ -0,0 +1,35 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/**
+ * @brief 
+ * Config类，用来读取配置文件，使用单例模式实现
+ * 在第一次调用 init 函数时会自动读取配置文件，也就是会自动调用 read() 函数
+ */
+
+#include <map>
+#include <string>
+
+class Config {
+private:
+    std::map<std::string, std::string> m_conf;      // 读取的配置文件内容以键值对的形式存储到 map 中
+    std::string m_conf_path;                        // 配置文件路径
+
+private:
+    Config();
+    ~Config();
+
+public:
+    void init(const char *conf_path);       // 初始化 config 类
+    void read();                            // 读取配置文件
+
+public:
+    // 单例模式
+    static Config *getInstance() {
+        static Config conf;
+        return &conf;
+    }
+};
+
+
+#endif // __CONFIG_H__
