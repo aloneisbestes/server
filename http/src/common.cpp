4f7d144178a6e150db6da7a76dd02a611c19c3af
aloneisbestes <aloneisbestes@gmail.com>
2022-06-29 22:25:21 +0800

    add function getconfigpaht

--- http/src/common.cpp
+++ http/src/common.cpp
@@ -1,5 +1,13 @@
-#include "common.h"
 #include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include "mexception.h"
+#include "common.h"
+#include "mmacro.h"
+
+using namespace http::macro;
 
 const char *strclr(char *src, char clr) {
     if (src == nullptr) 
@@ -37,4 +45,45 @@ const char *strclrs(char *src, const char *clrs) {
 
     *ret = '\0';
     return ret;
+}
+
+const char *getconfigpath(char *ret_path) {
+    // 获取配置文件路径
+    static char conf_path[FILE_PATH];
+
+    char *tmp = getcwd(conf_path, FILE_PATH);
+    if (tmp == nullptr) {
+        throw HttpException("配置文件路径获取失败。");
+        return nullptr;
+    }
+    printf("now_path: %s\n", tmp);
+
+    DIR *dir;
+    dirent *dp;
+    while (*conf_path != '\0') {    
+        // 打开目录
+        DIR *dir = opendir(conf_path);  // 打开目录
+        while ((dp = readdir(dir)) != nullptr) {
+            // 遍历该目录
+            if (strcmp(dp->d_name, CONF_NAME) == 0) {
+                strncat(conf_path, dp->d_name, strlen(dp->d_name));
+                if (ret_path != nullptr) 
+                    strcpy(ret_path, conf_path);
+
+                return conf_path;
+            }
+        }
+        closedir(dir); // 关闭目录
+
+        // tmp 不为空，并且conf_path配置文件路径的长度不为一，也就是根路径
+        tmp = strrchr(conf_path, '/');
+        if (tmp == nullptr) {
+            throw HttpException("配置文件路径获取失败。");
+            break;
+        }
+        *tmp = '\0';
+        printf("now_path: %s\n", conf_path);
+    }
+
+    return nullptr;
 }
