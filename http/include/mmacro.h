bb8811d2ca175422292a657096f8de1e5dff2aa0
aloneisbestes <aloneisbestes@gmail.com>
2022-06-29 22:24:53 +0800

    add mmacro.h

---
+++ http/include/mmacro.h
@@ -0,0 +1,14 @@
+#ifndef __MMACRO_H__
+#define __MMACRO_H__
+
+namespace http {
+    // 定义相关宏
+    namespace macro {
+        const int FILE_PATH = 4096;     // 文件路径的长度
+
+    }
+}
+
+#define CONF_NAME "conf"
+
+#endif // __MMACRO_H__
