29c0bb82f981e34f64eb707811db26b9bff1c63b
aloneisbestes <aloneisbestes@gmail.com>
2022-06-29 16:10:30 +0800

    update

--- http/include/mexecption.h
+++ http/include/mexecption.h
@@ -10,10 +10,26 @@ namespace http {
 
 class HttpException : public std::exception {
 public:
+    // 构造
     HttpException(const char *e) {
         m_exce = e;
     }
+    HttpException(const HttpException &exce) {
+        m_exce = exce.m_exce;
+    }
 
+    // 析构
+    virtual ~HttpException() { }
+
+    // 返回异常错误提示
+    virtual const char *what() const noexcept {
+        return m_exce.c_str();
+    }
+
+    // 等号运算符重载
+    HttpException &operator=(const HttpException &exce) noexcept {
+        m_exce = exce.m_exce;
+    }
 
 private:
     std::string m_exce;
