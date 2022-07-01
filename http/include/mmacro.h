a310f1b7743f6b4b75f2c5fffd2169c56ca7ff7b
aloneisbestes <aloneisbestes@gmail.com>
2022-07-01 16:11:02 +0800

    mmacro.h add BLOCK_QUEUE_SIZE

--- http/include/mmacro.h
+++ http/include/mmacro.h
@@ -9,7 +9,11 @@ namespace http {
     }
 }
 
+/* 定义配置文件名和配置文件路径 */
 #define CONF_NAME           "conf"
 #define CONF_FILE_NAME      "httpserver.conf"
+
+/* 定义阻塞队列的默认大小 */
+#define BLOCK_QUEUE_SIZE    1000
 
 #endif // __MMACRO_H__
