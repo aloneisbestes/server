0e699642616090cb8d7485c540e7f0f09e30891b
aloneisbestes <aloneisbestes@gmail.com>
2022-07-01 14:39:24 +0800

    test.cpp test add config

--- http/src/test.cpp
+++ http/src/test.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "common.h"
 #include "mexception.h"
+#include "mdebug.h"
+#include "config.h"
 
 
 int main() {
@@ -21,5 +23,14 @@ int main() {
         std::cout << e.what() << std::endl;
     }
     std::cout << str_tmp << std::endl;
+
+    Config::getInstance()->init(str_tmp);
+
+    auto conf = Config::getInstance()->getConfig();
+    for (auto it : conf) {
+        std::cout << it.first << ":" << it.second << std::endl;
+    }
+
+
     return 0;
 }
