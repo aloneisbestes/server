a47aef711468478e28817382428407daa617dd49
aloneisbestes <aloneisbestes@gmail.com>
2022-07-05 19:50:22 +0800

    update http

--- http/src/main.cpp
+++ http/src/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <string.h>
+#include "config.h"
+#include "log.h"
+#include "http.h"
+#include "common.h"
+#include "mdebug.h"
 
 /**
  * @brief 
@@ -10,6 +16,36 @@
 
 
 int main(int argc, char **argv) {
-    std::cout << "hello world.\n";
+
+    char conf_path[FILENAME_MAX]={0};
+    // 获取配置文件路径
+    try {
+        getconfigpath(conf_path);
+        DebugPrint("conf_path: %s\n", conf_path);
+    } catch (HttpException e) {
+        std::cout << e.what() << std::endl;
+        strcpy(conf_path, "/home/alone/work/project/server/conf");
+    }
+
+    // 初始化配置文件
+    try {
+        Config::getInstance()->init(conf_path);
+    } catch (HttpException e) {
+        std::cout << e.what() << std::endl;
+    }
+    
+    // 初始化log日志文件
+    try{
+        Log::get()->init("http", false);
+    } catch (HttpException e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    Log::get()->write(INFO_TYPE, "init complete.");
+
+    // 开启服务器
+    HttpSocket *httpserver = new HttpSocket();
+    httpserver->start();
+
     return 0;
 }
